b612643aefe91f142d069e0c3dc4009bae23542e
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-14 18:44:04 -0600

    Failed attmept at search query

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,26 +9,19 @@ class Merchant < ApplicationRecord
   #   @id = data[:id]
   # end
 
-
-
-     def self.find_one(params)
-       attributes = Merchant.first.attributes
-
-       search_words = params.permit(attributes.keys).to_h
-
-       answer = search_words.map do |k,v|
-
-
-         binding.pry
-          Merchant.where("lower(#{k}) LIKE '%#{v.downcase}%'")
-       end
+  
 
 
 
-
-     end
-
-    def self.find_all(params)
-
-    end
+    #  def self.find_one(params)
+    #    attributes = Merchant.first.attributes
+    #    search_words = params.permit(attributes.keys).to_h
+    #    answer = search_words.map do |k,v|
+    #       Merchant.where("lower(#{k}) LIKE '%#{v.downcase}%'")
+    #    end
+    #  end
+    #
+    # def self.find_all(params)
+    #
+    # end
 end
