7de44bee87f094994b2da7866d1c7233c80bc993
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-15 18:50:03 -0600

    Created test for buisness end points

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :invoice_items do
+  factory :invoice_item do
     quantity { Faker::Number.between(from: 1, to: 5) }
    unit_price { Faker::Number.decimal(l_digits: 2) }
    invoice
--- spec/requests/api/v1/buisness_endpoints_spec.rb
+++ spec/requests/api/v1/buisness_endpoints_spec.rb
@@ -5,18 +5,20 @@ describe 'Business API Endpoints' do
     @merchant2 = create(:merchant)
     @merchant3 = create(:merchant)
 
+    @customer1 = create(:customer)
+
     @item1 = create(:item, merchant: @merchant1, unit_price: 1.50)
     @item2 = create(:item, merchant: @merchant1, unit_price: 3.00)
     @item3 = create(:item, merchant: @merchant2, unit_price: 5.25)
     @item4 = create(:item, merchant: @merchant2, unit_price: 5.50)
     @item5 = create(:item, merchant: @merchant3, unit_price: 15.30)
 
-    @invoice1 = create(:invoice, merchant: @merchant1, created_at: '2012-03-20 14:54:09 UTC')
-    @invoice2 = create(:invoice, merchant: @merchant1, created_at: '2012-03-27 14:54:09 UTC')
-    @invoice3 = create(:invoice, merchant: @merchant2, created_at: '2012-03-27 14:54:09 UTC')
-    @invoice4 = create(:invoice, merchant: @merchant2, created_at: '2012-03-24 14:54:09 UTC')
-    @invoice5 = create(:invoice, merchant: @merchant3, created_at: '2012-03-27 14:54:09 UTC')
-    @invoice6 = create(:invoice, merchant: @merchant3, created_at: '2012-03-29 14:54:09 UTC')
+    @invoice1 = create(:invoice, merchant_id: @merchant1.id, created_at: '2012-03-20 14:54:09 UTC', customer_id: @customer1.id)
+    @invoice2 = create(:invoice, merchant_id: @merchant1.id, created_at: '2012-03-27 14:54:09 UTC', customer_id: @customer1.id)
+    @invoice3 = create(:invoice, merchant_id: @merchant2.id, created_at: '2012-03-27 14:54:09 UTC', customer_id: @customer1.id)
+    @invoice4 = create(:invoice, merchant_id: @merchant2.id, created_at: '2012-03-24 14:54:09 UTC', customer_id: @customer1.id)
+    @invoice5 = create(:invoice, merchant_id: @merchant3.id, created_at: '2012-03-27 14:54:09 UTC', customer_id: @customer1.id)
+    @invoice6 = create(:invoice, merchant_id: @merchant3.id, created_at: '2012-03-29 14:54:09 UTC', customer_id: @customer1.id)
 
     @invoice_item1 = create(:invoice_item, item: @item1, invoice: @invoice1, quantity: 1, unit_price: 1.50)
     @invoice_item2 = create(:invoice_item, item: @item2, invoice: @invoice1, quantity: 1, unit_price: 3.00)
@@ -33,4 +35,14 @@ describe 'Business API Endpoints' do
   end
 
   it "can display merchant with most revenue" do
+    get "/api/v1/merchants/most_revenue?quantity=2"
+    json = JSON.parse(response.body, symbolize_name: true)
+
+    binding.pry
+
+    
+
+    #3 highest
+    #1 next
+  end
 end
